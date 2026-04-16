95d72d71def6fe5db2fe73085d4c97acff5c805f
EngineLin <linengine@gmail.com>
2026-04-16 19:28:15 +0700

    新增 console log

--- task-update-master.js
+++ task-update-master.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 const path = require('path');
 
+console.log('task-update-master.js');
+
 const taskUpdateMaster = () => {
     const taskUpdateMasterPath = path.join(__dirname, 'task-update-master.js');
     const taskUpdateMasterContent = fs.readFileSync(taskUpdateMasterPath, 'utf8');
