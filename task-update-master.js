8fcc19103f9dd727a8340f47aee214e59c08cac2
EngineLin <linengine@gmail.com>
2026-04-17 08:42:34 +0700

    #10000-超過一萬號

--- task-update-master.js
+++ task-update-master.js
@@ -4,8 +4,6 @@ const path = require('path');
 console.log('task-update-master.js');
 
 const taskUpdateMaster = () => {
-    const taskUpdateMasterPath = path.join(__dirname, 'task-update-master.js');
-    const taskUpdateMasterContent = fs.readFileSync(taskUpdateMasterPath, 'utf8');
     console.log(taskUpdateMasterContent);
     console.log('taskUpdateMaster');
 }
